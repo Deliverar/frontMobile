9a89d3f997eca57338ee35cea90eacc9102fd7d4
Lucas Miño <lucas@xoolix.com>
2023-10-27 03:42:08 -0300

    test cypress

---
+++ cypress/e2e/TestMobile.cy.js
@@ -0,0 +1,8 @@
+describe('template spec', () => {
+  it('Test_1', () => {
+    cy.visit('http://localhost:19006/')
+    /* ==== Generated with Cypress Studio ==== */
+   
+    /* ==== End Cypress Studio ==== */
+  })
+})
